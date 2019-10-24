4686463ba6e2456b4b58a6181c089f275951e2a5
John Doe <jdoe@email.com>
2019-10-24 21:40:09 +0800

    init

--- packages/wxmp/pages/index/index.js
+++ packages/wxmp/pages/index/index.js
@@ -1,7 +1,6 @@
 import {dispatch, page} from '../../mpsm/index'
 import diff from '../../mpsm/diff'
 import {jumpTo} from '../../utils/util'
-import getIndexAlbums from '../../services/apis/getIndexAlbums'
 
 page({
   watch: {
@@ -23,13 +22,7 @@ page({
     }
   },
   onLoad() {
-    const r = diff({
-      a: 1, b: 2, c: "str", d: { e: [2, { a: 4 }, 5] }, f: true, h: [1], g: { a: [1, 2], j: 111 }
-    }, {
-      a: [], b: "aa", c: 3, d: { e: [3, { a: 3 }] }, f: false, h: [1, 2], g: { a: [1, 1, 1], i: "delete" }, k: 'del'
-    })
-    console.log(r)
-    // getIndexAlbums().then(albums => console.log(albums))
+
   },
   onShow() {
     console.log('上一页路由', this.data.prev)
